6b7cdb9abf0f5e26bf8c370f4cfa3a1653e5f762
Sazid Hasan Milon <36078194+shmilon@users.noreply.github.com>
2023-07-01 01:03:45 +0600

    Update db.sql

--- webapp/db.sql
+++ webapp/db.sql
@@ -10,13 +10,3 @@ CREATE TABLE `members` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 
-CREATE TABLE `members_new` (
-  `memberID` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `active` varchar(255) NOT NULL,
-  `resetToken` varchar(255) DEFAULT NULL,
-  `resetComplete` varchar(3) DEFAULT 'No',
-  PRIMARY KEY (`memberID`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
